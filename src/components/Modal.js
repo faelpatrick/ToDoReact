33c0e51fd350e77cf40ec77cb4bc7452308f0974
faelpatrick <faelpatrick@gmail.com>
2021-10-03 18:50:18 -0300

    Improve the layout and separated the components

---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import { useState } from "react/cjs/react.development";
+import Card from "./Card";
+
+function Modal(props) {
+
+    // const [show, setShow] = useState(false)
+
+    function hideModal(e) {
+        let target = e.target;
+        if (target.id === 'modal') {
+            props.onHideModal()
+         }
+    }
+
+    return (<div id="modal" onClick={hideModal} className={props.show ? "modal" : "modal hide"}>
+        <Card className="cardModal">
+            {props.children}
+        </Card>
+    </div>);
+}
+
+export default Modal;
